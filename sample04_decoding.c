cefaca5fd7530ddb6e6edc143612c9188c9f109c
Lee <inquisitor@airensoft.com>
2015-06-25 14:46:57 +0900

    코드 정리

--- sample04_decoding.c
+++ sample04_decoding.c
@@ -10,76 +10,64 @@ const char* inFileName;
 int videoStreamIndex = -1;
 int audioStreamIndex = -1;
 
-static int AVOpenDecoder(AVCodecContext* avCodecContext)
-{
-	if(avCodecContext == NULL)
-	{
-		return -1;
-	}
-
-	// 코덱 ID를 통해 FFmpeg 라이브러리가 자동으로 코덱을 찾도록 합니다.
-	AVCodec* decoder = avcodec_find_decoder(avCodecContext->codec_id);
-	if(decoder == NULL)
-	{
-		return -2;
-	}
-
-	// 찾아낸 디코더를 통해 코덱을 엽니다.
-	if(avcodec_open2(avCodecContext, decoder, NULL) < 0)
-	{
-		return -3;
-	}
-
-	return 0;
-}
-
-static int AVOpenInput(const char* fileName)
+static int openInputFile()
 {
 	unsigned int index;
-	int returnCode;
-
-	returnCode = avformat_open_input(&inAVFormatContext, fileName, NULL, NULL);
+	int returnCode = avformat_open_input(&inAVFormatContext, inFileName, NULL, NULL);
 	if(returnCode < 0)
 	{
-		printf("알려지지 않았거나 잘못된 파일 형식입니다.\n");
+		fprintf(stderr, "Could not open input file %s\n", inFileName);
 		return -1;
 	}
 
-	//주어진 AVFormatContext로부터 유효한 스트림이 있는지 찾습니다.
 	returnCode = avformat_find_stream_info(inAVFormatContext, NULL);
 	if(returnCode < 0)
 	{
-		printf("유효한 스트림 정보가 없습니다.\n");
+		fprintf(stderr, "Failed to retrieve input stream information\n");
 		return -2;
 	}
 
 	for(index = 0; index < inAVFormatContext->nb_streams; index++)
 	{
 		AVCodecContext* avCodecContext = inAVFormatContext->streams[index]->codec;
-		if(avCodecContext->codec_type == AVMEDIA_TYPE_VIDEO)
+		if(avCodecContext->codec_type == AVMEDIA_TYPE_VIDEO || avCodecContext->codec_type == AVMEDIA_TYPE_AUDIO)
 		{
-			returnCode = AVOpenDecoder(avCodecContext);
-			if(returnCode < 0) break;
-			videoStreamIndex = index;
-		}
-		else if(avCodecContext->codec_type == AVMEDIA_TYPE_AUDIO)
-		{
-			returnCode = AVOpenDecoder(avCodecContext);
-			if(returnCode < 0) break;
-			audioStreamIndex = index;
+			// 코덱 ID를 통해 FFmpeg 라이브러리가 자동으로 코덱을 찾도록 합니다.
+			AVCodec* decoder = avcodec_find_decoder(avCodecContext->codec_id);
+			if(decoder == NULL)
+			{
+				fprintf(stderr, "Could not find decoder for stream #%d\n", index);
+				break;
+			}
+
+			// 찾아낸 디코더를 통해 코덱을 엽니다.
+			if(avcodec_open2(avCodecContext, decoder, NULL) < 0)
+			{
+				fprintf(stderr, "Failed opening codec for stream #%d\n", index);
+				break;
+			}
+
+			if(avCodecContext->codec_type == AVMEDIA_TYPE_VIDEO)
+			{
+				videoStreamIndex = index;
+			}
+			else
+			{
+				audioStreamIndex = index;
+			}
 		}
 	}
 
 	if(videoStreamIndex < 0 && audioStreamIndex < 0)
 	{
-		printf("이 컨테이너에는 비디오/오디오 스트림 정보가 없습니다.\n");
+		fprintf(stderr, "Failed to retrieve input stream information\n");
 		return -3;
 	}
 
 	return 0;
 }
 
-static void AVRelease()
+static void release()
 {
 	if(inAVFormatContext != NULL)
 	{
@@ -87,15 +75,14 @@ static void AVRelease()
 	}
 }
 
-static int decodePacket(AVCodecContext* avCodecContext, const enum AVMediaType streamType, AVPacket* packet, AVFrame** frame, int* gotFrame)
+static int decodePacket(AVCodecContext* avCodecContext, AVPacket* packet, AVFrame** frame, int* gotFrame)
 {
 	int (*decodeFunc)(AVCodecContext *, AVFrame *, int *, const AVPacket *);
 	int decodedPacketSize;
 	
 	*gotFrame = 0;
 
-	decodeFunc = (streamType == AVMEDIA_TYPE_VIDEO) ? avcodec_decode_video2 : avcodec_decode_audio4;
-
+	decodeFunc = (avCodecContext->codec_type == AVMEDIA_TYPE_VIDEO) ? avcodec_decode_video2 : avcodec_decode_audio4;
 	decodedPacketSize = decodeFunc(avCodecContext, *frame, gotFrame, packet);
 	if(*gotFrame)
 	{
@@ -113,24 +100,22 @@ int main(int argc, char* argv[])
 
 	if(argc < 2)
 	{
-		printf("파일 이름을 입력하세요.\n");
+		fprintf(stderr, "usage : %s <input>\n", argv[0]);
 		exit(EXIT_SUCCESS);
 	}
 
 	inFileName = argv[1];
-
-	returnCode = AVOpenInput(inFileName);
+	returnCode = openInputFile();
 	if(returnCode < 0)
 	{
-		AVRelease();
+		release();
 		exit(EXIT_SUCCESS);
 	}
 
-	//디코딩된 프레임이 저장되는 곳입니다.
 	AVFrame* decodedFrame = av_frame_alloc();
 	if(decodedFrame == NULL)
 	{
-		AVRelease();
+		release();
 		exit(EXIT_SUCCESS);
 	}
 
@@ -141,6 +126,7 @@ int main(int argc, char* argv[])
 		returnCode = av_read_frame(inAVFormatContext, &packet);
 		if(returnCode == AVERROR_EOF)
 		{
+			fprintf(stdout, "End of frame\n");
 			break;
 		}
 
@@ -148,35 +134,32 @@ int main(int argc, char* argv[])
 		if(avCodecContext == NULL) continue;
 		
 		const enum AVMediaType streamType = avCodecContext->codec_type;
-
 		int gotFrame;
 
-		returnCode = decodePacket(avCodecContext, streamType, &packet, &decodedFrame, &gotFrame);
-		if(gotFrame)
+		returnCode = decodePacket(avCodecContext, &packet, &decodedFrame, &gotFrame);
+		if(returnCode >= 0 && gotFrame)
 		{
-			printf("-----------------------\n");
+			fprintf(stdout, "-----------------------\n");
 			if(streamType == AVMEDIA_TYPE_VIDEO)
 			{
-				printf("[VIDEO] frame->width, height : %dx%d\n", decodedFrame->width, decodedFrame->height);
-				printf("[VIDEO] frame->sample_aspect_ratio : %d/%d\n", decodedFrame->sample_aspect_ratio.num, decodedFrame->sample_aspect_ratio.den);
+				fprintf(stdout, "Video : frame->width, height : %dx%d\n", decodedFrame->width, decodedFrame->height);
+				fprintf(stdout, "Video : frame->sample_aspect_ratio : %d/%d\n", decodedFrame->sample_aspect_ratio.num, decodedFrame->sample_aspect_ratio.den);
 			}
 			else
 			{
-				printf("[AUDIO] frame->nb_samples : %d\n", decodedFrame->nb_samples);
-				printf("[AUDIO] frame->channels : %d\n", decodedFrame->channels);
+				fprintf(stdout, "Audio : frame->nb_samples : %d\n", decodedFrame->nb_samples);
+				fprintf(stdout, "Audio : frame->channels : %d\n", decodedFrame->channels);
 			}
 
-			// AVFrame의 내부 버퍼만 해제합니다.
 			av_frame_unref(decodedFrame);
 		}
 
 		av_free_packet(&packet);
 	}
 
-	//av_frame_alloc으로 할당된 AVFrame을 해제
 	av_frame_free(&decodedFrame);
 
-	AVRelease();
+	release();
 
 	return 0;
 }
