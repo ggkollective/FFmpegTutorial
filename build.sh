c7792136e952aeb30f4491d19d775707733c1621
Lee <inquisitor@airensoft.com>
2015-06-25 12:38:43 +0900

    sample01 리팩토링, 불필요한 코드 삭제

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-gcc -g -o sample01_scaning sample01_scaning.c -I"/opt/ffmpeg/include" $(pkg-config --libs libavformat libavcodec libavutil libavfilter);
-gcc -g -o sample02_demuxing sample02_demuxing.c -I"/opt/ffmpeg/include" $(pkg-config --libs libavformat libavcodec libavutil libavfilter);
-gcc -g -o sample03_remuxing sample03_remuxing.c -I"/opt/ffmpeg/include" $(pkg-config --libs libavformat libavcodec libavutil libavfilter);
-gcc -g -o sample04_decoding sample04_decoding.c -I"/opt/ffmpeg/include" $(pkg-config --libs libavformat libavcodec libavutil libavfilter);
+gcc -g -o sample01_scaning sample01_scaning.c -I"/opt/ffmpeg/include" $(pkg-config --libs libavformat libavcodec libavutil);
+gcc -g -o sample02_demuxing sample02_demuxing.c -I"/opt/ffmpeg/include" $(pkg-config --libs libavformat libavcodec libavutil);
+gcc -g -o sample03_remuxing sample03_remuxing.c -I"/opt/ffmpeg/include" $(pkg-config --libs libavformat libavcodec libavutil);
+gcc -g -o sample04_decoding sample04_decoding.c -I"/opt/ffmpeg/include" $(pkg-config --libs libavformat libavcodec libavutil);
+gcc -g -o sample05_audio_encoding sample05_audio_encoding.c -I"/opt/ffmpeg/include" $(pkg-config --libs libavformat libavcodec libavutil libavfilter);
--- sample01_scaning.c
+++ sample01_scaning.c
@@ -5,27 +5,24 @@
 AVFormatContext* inAVFormatContext = NULL;
 const char* inFileName;
 
-static int AVOpenInput(const char* fileName)
+static int openInputFile()
 {
-	int returnCode = avformat_open_input(&inAVFormatContext, fileName, NULL, NULL);
+	int returnCode = avformat_open_input(&inAVFormatContext, inFileName, NULL, NULL);
 	if(returnCode < 0)
 	{
-		printf("알려지지 않았거나 잘못된 파일 형식입니다.\n");
 		return -1;
 	}
 
-	//주어진 AVFormatContext로부터 유효한 스트림이 있는지 찾습니다.
 	returnCode = avformat_find_stream_info(inAVFormatContext, NULL);
 	if(returnCode < 0)
 	{
-		printf("유효한 스트림 정보가 없습니다.\n");
 		return -2;
 	}
 
 	return 0;
 }
 
-static void AVRelease()
+static void release()
 {
 	if(inAVFormatContext != NULL)
 	{
@@ -35,70 +32,44 @@ static void AVRelease()
 
 int main(int argc, char* argv[])
 {
+	unsigned int index;
 	int returnCode;
 
-	// 컨테이너, 코덱 정보를 일괄적으로 등록합니다.
-	// 이 함수는 여러번 호출되어도 내부적으로는 한번만 등록됩니다.
 	av_register_all();
 
 	if(argc < 2)
 	{
-		printf("파일 이름을 입력하세요.\n");
 		exit(EXIT_SUCCESS);
 	}
 
 	inFileName = argv[1];
-	// 주어진 파일 이름으로부터 AVFormatContext를 가져옵니다.
-	returnCode = AVOpenInput(inFileName);
+	returnCode = openInputFile();
 	if(returnCode < 0)
 	{
-		AVRelease();
-		exit(EXIT_SUCCESS);
+		release();
+		return 0;
 	}
 
-	unsigned int index;
-
-	// 스트림 index를 미리 저장하기 위해 사용합니다.
-	// 이 정보는 리먹싱, 디코딩과 인코딩시 데이터를 추적하는데 유용하게 사용할 수 있습니다.
-	int videoStreamIndex = -1;
-	int audioStreamIndex = -1;
-
-	// inAVFormatContext->nb_streams : 컨테이너가 저장하고 있는 총 스트림 갯수
 	for(index = 0; index < inAVFormatContext->nb_streams; index++)
 	{
-		// 이 과정에서 스트림 내부에 있는 코덱 정보를 가져올 수 있습니다.
-		AVCodecContext* pCodecContext = inAVFormatContext->streams[index]->codec;
-
-		// pCodecContext->codec_type : 코덱의 타입을 알 수 있습니다.
-		// 가장 많이 볼 수 있는 타입은 비디오와 오디오입니다.
-		if(pCodecContext->codec_type == AVMEDIA_TYPE_VIDEO)
+		AVCodecContext* avCodecContext = inAVFormatContext->streams[index]->codec;
+		if(avCodecContext->codec_type == AVMEDIA_TYPE_VIDEO)
 		{
-			// 이 컨테이너에는 적어도 한개 이상의 비디오 코덱정보가 있습니다.
-			videoStreamIndex = index;
-
-			printf("------- video information -------\n");
-			printf("codec_id : %d\n", pCodecContext->codec_id);
-			printf("bitrate : %d\n", pCodecContext->bit_rate);
-			printf("width : %d / height : %d\n", pCodecContext->width, pCodecContext->height);
+			printf("------- Video info -------\n");
+			printf("codec_id : %d\n", avCodecContext->codec_id);
+			printf("bitrate : %d\n", avCodecContext->bit_rate);
+			printf("width : %d / height : %d\n", avCodecContext->width, avCodecContext->height);
 		}
-		else if(pCodecContext->codec_type == AVMEDIA_TYPE_AUDIO)
+		else if(avCodecContext->codec_type == AVMEDIA_TYPE_AUDIO)
 		{
-			// 이 컨테이너에는 적어도 한개 이상의 오디오 코덱정보가 있습니다.
-			audioStreamIndex = index;
-
-			printf("-------Audio information -------\n");
-			printf("codec_id : %d\n", pCodecContext->codec_id);
-			printf("bitrate : %d\n", pCodecContext->bit_rate);
-			printf("sample_rate : %d\n", pCodecContext->sample_rate);
-			printf("number of channels : %d\n", pCodecContext->channels);
+			printf("------- Audio info -------\n");
+			printf("codec_id : %d\n", avCodecContext->codec_id);
+			printf("bitrate : %d\n", avCodecContext->bit_rate);
+			printf("sample_rate : %d\n", avCodecContext->sample_rate);
+			printf("number of channels : %d\n", avCodecContext->channels);
 		}
 	}
 
-	if(videoStreamIndex < 0 && audioStreamIndex < 0)
-	{
-		printf("이 컨테이너에는 비디오/오디오 스트림 정보가 없습니다.\n");
-	}
-
-	AVRelease();
+	release();
 	return 0;
 }
