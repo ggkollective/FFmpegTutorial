2842043d3d2f3d38fc692485db2be72995bbe9fb
Lee <inquisitor@airensoft.com>
2015-06-27 16:32:25 +0900

    필터링 소스 추가

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ gcc -g -o sample01_scaning sample01_scaning.c -I"/opt/ffmpeg/include" $(pkg-conf
 gcc -g -o sample02_demuxing sample02_demuxing.c -I"/opt/ffmpeg/include" $(pkg-config --libs libavformat libavcodec libavutil);
 gcc -g -o sample03_remuxing sample03_remuxing.c -I"/opt/ffmpeg/include" $(pkg-config --libs libavformat libavcodec libavutil);
 gcc -g -o sample04_decoding sample04_decoding.c -I"/opt/ffmpeg/include" $(pkg-config --libs libavformat libavcodec libavutil);
-gcc -g -o sample05_audio_encoding sample05_audio_encoding.c -I"/opt/ffmpeg/include" $(pkg-config --libs libavformat libavcodec libavutil libavfilter);
+gcc -g -o sample05_audio_filtering sample05_audio_filtering.c -I"/opt/ffmpeg/include" $(pkg-config --libs libavformat libavcodec libavutil libavfilter);
