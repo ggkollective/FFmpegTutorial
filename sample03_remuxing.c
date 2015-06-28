15711d6d19de21869fb182f6f55045a191fcdb39
Lee <inquisitor@airensoft.com>
2015-06-28 23:06:10 +0900

    sample03 리팩토링

--- sample03_remuxing.c
+++ sample03_remuxing.c
@@ -2,45 +2,54 @@
 #include <libavcodec/avcodec.h>
 #include <stdio.h>
 
-AVFormatContext* inAVFormatContext = NULL;
-AVFormatContext* outAVFormatContext = NULL;
-const char* inFileName;
-const char* outFileName;
+typedef struct _FileContext
+{
+	AVFormatContext* avFormatContext;
+	const char* fileName;
+	int videoIndex;
+	int audioIndex;
+} FileContext;
 
-int videoStreamIndex = -1;
-int audioStreamIndex = -1;
+static FileContext inputFile, outputFile;
 
-static int openInputFile()
+static int openInputFile(const char* fileName)
 {
 	unsigned int index;
-	int returnCode = avformat_open_input(&inAVFormatContext, inFileName, NULL, NULL);
+	int returnCode;
+
+	inputFile.avFormatContext = NULL;
+	inputFile.fileName = fileName;
+	inputFile.audioIndex = -1;
+	inputFile.videoIndex = -1;
+
+	returnCode = avformat_open_input(&inputFile.avFormatContext, inputFile.fileName, NULL, NULL);
 	if(returnCode < 0)
 	{
-		fprintf(stderr, "Could not open input file %s\n", inFileName);
+		fprintf(stderr, "Could not open input file %s\n", inputFile.fileName);
 		return -1;
 	}
 
-	returnCode = avformat_find_stream_info(inAVFormatContext, NULL);
+	returnCode = avformat_find_stream_info(inputFile.avFormatContext, NULL);
 	if(returnCode < 0)
 	{
 		fprintf(stderr, "Failed to retrieve input stream information\n");
 		return -2;
 	}
 
-	for(index = 0; index < inAVFormatContext->nb_streams; index++)
+	for(index = 0; index < inputFile.avFormatContext->nb_streams; index++)
 	{
-		AVCodecContext* avCodecContext = inAVFormatContext->streams[index]->codec;
-		if(avCodecContext->codec_type == AVMEDIA_TYPE_VIDEO && videoStreamIndex < 0)
+		AVCodecContext* avCodecContext = inputFile.avFormatContext->streams[index]->codec;
+		if(avCodecContext->codec_type == AVMEDIA_TYPE_VIDEO && inputFile.videoIndex < 0)
 		{
-			videoStreamIndex = index;
+			inputFile.videoIndex = index;
 		}
-		else if(avCodecContext->codec_type == AVMEDIA_TYPE_AUDIO && audioStreamIndex < 0)
+		else if(avCodecContext->codec_type == AVMEDIA_TYPE_AUDIO && inputFile.audioIndex < 0)
 		{
-			audioStreamIndex = index;
+			inputFile.audioIndex = index;
 		}
-	}
+	} // for
 
-	if(videoStreamIndex < 0 && audioStreamIndex < 0)
+	if(inputFile.videoIndex < 0 && inputFile.audioIndex < 0)
 	{
 		fprintf(stderr, "Failed to retrieve input stream information\n");
 		return -3;
@@ -49,58 +58,67 @@ static int openInputFile()
 	return 0;
 }
 
-static int createOutputFile()
+static int createOutputFile(const char* fileName)
 {
 	unsigned int index;
-	int returnCode = avformat_alloc_output_context2(&outAVFormatContext, NULL, NULL, outFileName);
+	int returnCode;
+
+	outputFile.avFormatContext = NULL;
+	outputFile.fileName = fileName;
+
+	returnCode = avformat_alloc_output_context2(&outputFile.avFormatContext, NULL, NULL, outputFile.fileName);
 	if(returnCode < 0)
 	{
 		fprintf(stderr, "Could not create output context\n");
 		return -1;
 	}
 
-	for(index = 0; index < inAVFormatContext->nb_streams; index++)
+	for(index = 0; index < inputFile.avFormatContext->nb_streams; index++)
 	{
-		AVStream* inStream = inAVFormatContext->streams[index];
-		AVCodecContext* inCodecContext = inStream->codec;
-
-		AVStream* outStream = avformat_new_stream(outAVFormatContext, inCodecContext->codec);
-		if(outStream == NULL)
+		// Input 파일로부터 읽어드린 스트림만 추가합니다.
+		if(index == inputFile.videoIndex || index == inputFile.audioIndex)
 		{
-			fprintf(stderr, "Failed to allocate output stream\n");
-			return -2;
-		}
-		AVCodecContext* outCodecContext = outStream->codec;
+			AVStream* inAVStream = inputFile.avFormatContext->streams[index];
+			AVCodecContext* inAVCodecContext = inAVStream->codec;
 
-		returnCode = avcodec_copy_context(outCodecContext, inCodecContext);
-		if(returnCode < 0)
-		{
-			fprintf(stderr, "Error occurred while copying context\n");
-			return -3;
-		}
+			AVStream* outAVStream = avformat_new_stream(outputFile.avFormatContext, inAVCodecContext->codec);
+			if(outAVStream == NULL)
+			{
+				fprintf(stderr, "Failed to allocate output stream\n");
+				return -2;
+			}
+			AVCodecContext* outCodecContext = outAVStream->codec;
 
-		// Deprecated된 AVCodecContext 대신 AVStream을 사용.
-		outStream->time_base = inStream->time_base;
-		// FFmpeg에서 지원하는 코덱과 호환성을 맞추기 위해 코덱 태그정보 삭제
-		outCodecContext->codec_tag = 0;
+			returnCode = avcodec_copy_context(outCodecContext, inAVCodecContext);
+			if(returnCode < 0)
+			{
+				fprintf(stderr, "Error occurred while copying context\n");
+				return -3;
+			}
 
-		if(outAVFormatContext->oformat->flags & AVFMT_GLOBALHEADER)
-		{
-			outCodecContext->flags |= CODEC_FLAG_GLOBAL_HEADER;
-		}
-	}
+			// Deprecated된 AVCodecContext 대신 AVStream을 사용.
+			outAVStream->time_base = inAVStream->time_base;
+			// FFmpeg에서 지원하는 코덱과 호환성을 맞추기 위해 코덱 태그정보 삭제
+			outCodecContext->codec_tag = 0;
 
-	if(!(outAVFormatContext->oformat->flags & AVFMT_NOFILE))
+			if(outputFile.avFormatContext->oformat->flags & AVFMT_GLOBALHEADER)
+			{
+				outCodecContext->flags |= CODEC_FLAG_GLOBAL_HEADER;
+			}
+		} // if
+	} // for
+
+	if(!(outputFile.avFormatContext->oformat->flags & AVFMT_NOFILE))
 	{
 		// 실질적으로 파일을 오픈하는 시점입니다.
-		if(avio_open(&outAVFormatContext->pb, outFileName, AVIO_FLAG_WRITE) < 0)
+		if(avio_open(&outputFile.avFormatContext->pb, outputFile.fileName, AVIO_FLAG_WRITE) < 0)
 		{
-			fprintf(stderr, "Failed to create output file %s\n", outFileName);
+			fprintf(stderr, "Failed to create output file %s\n", outputFile.fileName);
 			return -4;
 		}
 	}
 
-	returnCode = avformat_write_header(outAVFormatContext, NULL);
+	returnCode = avformat_write_header(outputFile.avFormatContext, NULL);
 	if(returnCode < 0)
 	{
 		fprintf(stderr, "Failed writing header into output file\n");
@@ -112,18 +130,18 @@ static int createOutputFile()
 
 static void release()
 {
-	if(inAVFormatContext != NULL)
+	if(inputFile.avFormatContext != NULL)
 	{
-		avformat_close_input(&inAVFormatContext);
+		avformat_close_input(&inputFile.avFormatContext);
 	}
 
-	if(outAVFormatContext != NULL)
+	if(outputFile.avFormatContext != NULL)
 	{
-		if(!(outAVFormatContext->oformat->flags & AVFMT_NOFILE))
+		if(!(outputFile.avFormatContext->oformat->flags & AVFMT_NOFILE))
 		{
-			avio_closep(&outAVFormatContext->pb);
+			avio_closep(&outputFile.avFormatContext->pb);
 		}
-		avformat_free_context(outAVFormatContext);
+		avformat_free_context(outputFile.avFormatContext);
 	}
 }
 
@@ -139,16 +157,14 @@ int main(int argc, char* argv[])
 		exit(EXIT_SUCCESS);
 	}
 
-	inFileName = argv[1];
-	returnCode = openInputFile();
+	returnCode = openInputFile(argv[1]);
 	if(returnCode < 0)
 	{
 		release();
 		exit(EXIT_SUCCESS);
 	}
 
-	outFileName = argv[2];
-	returnCode = createOutputFile();
+	returnCode = createOutputFile(argv[2]);
 	if(returnCode < 0)
 	{
 		release();
@@ -156,36 +172,39 @@ int main(int argc, char* argv[])
 	}
 
 	// OUTPUT 파일에 대한 정보를 출력합니다.
-	av_dump_format(outAVFormatContext, 0, outFileName, 1);
+	av_dump_format(outputFile.avFormatContext, 0, outputFile.fileName, 1);
 
 	AVPacket packet;
 
 	while(1)
 	{
-		returnCode = av_read_frame(inAVFormatContext, &packet);
+		returnCode = av_read_frame(inputFile.avFormatContext, &packet);
 		if(returnCode == AVERROR_EOF)
 		{
 			fprintf(stdout, "End of frame\n");
 			break;
 		}
 
-		AVStream* inStream = inAVFormatContext->streams[packet.stream_index];
-		AVStream* outStream = outAVFormatContext->streams[packet.stream_index];
-
-		av_packet_rescale_ts(&packet, inStream->time_base, outStream->time_base);
-
-		returnCode = av_interleaved_write_frame(outAVFormatContext, &packet);
-		if(returnCode < 0)
+		if(packet.stream_index == inputFile.videoIndex || packet.stream_index == inputFile.audioIndex)
 		{
-			fprintf(stderr, "Error occurred when writing packet into file\n");
-			break;
-		}
+			AVStream* inAVStream = inputFile.avFormatContext->streams[packet.stream_index];
+			AVStream* outAVtStream = outputFile.avFormatContext->streams[packet.stream_index];
+
+			av_packet_rescale_ts(&packet, inAVStream->time_base, outAVtStream->time_base);
+
+			returnCode = av_interleaved_write_frame(outputFile.avFormatContext, &packet);
+			if(returnCode < 0)
+			{
+				fprintf(stderr, "Error occurred when writing packet into file\n");
+				break;
+			}
+		} // if
 
 		av_free_packet(&packet);
-	}
+	} // while
 
 	// 파일을 쓰는 시점에서 마무리하지 못한 정보를 정리하는 시점입니다.
-	av_write_trailer(outAVFormatContext);
+	av_write_trailer(outputFile.avFormatContext);
 
 	release();
 
