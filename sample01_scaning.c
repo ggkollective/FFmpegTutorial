2a02fd5caa011cd2ab5a148e625b1133099c13bf
Lee <sorrowhill@users.noreply.github.com>
2015-10-24 16:49:12 +0900

    add comments in english

--- sample01_scaning.c
+++ sample01_scaning.c
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
 
 	av_register_all();
 
-	// FFmpeg 라이브러리 레벨에서 디버깅 로그를 출력하도록 합니다.
+	// Print debug log in library level.
 	av_log_set_level(AV_LOG_DEBUG);
 
 	if(argc < 2)
@@ -19,21 +19,21 @@ int main(int argc, char* argv[])
 		return 0;
 	}
 
-	// 주어진 파일 이름으로부터 fmt_ctx를 가져옵니다. 
+	// Get fmt_ctx from given file path. 
 	if(avformat_open_input(&fmt_ctx, argv[1], NULL, NULL) < 0)
 	{
 		printf("Could not open input file %s\n", argv[1]);
 		return -1;
 	}
 
-	//주어진 fmt_ctx로부터 유효한 스트림이 있는지 찾습니다.
+	// Find stream information from given fmt_ctx.
 	if(avformat_find_stream_info(fmt_ctx, NULL) < 0)
 	{
 		printf("Failed to retrieve input stream information\n");
 		return -2;
 	}
 
-	// fmt_ctx->nb_streams : 컨테이너가 저장하고 있는 총 스트림 갯수
+	// fmt_ctx->nb_streams : number of total streams in video file.
 	for(index = 0; index < fmt_ctx->nb_streams; index++)
 	{
 		AVCodecContext* avCodecContext = fmt_ctx->streams[index]->codec;
