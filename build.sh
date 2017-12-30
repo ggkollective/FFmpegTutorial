bdcc17ec645da026d4bbd38e239acff5e2f7d62a
Lee <sorrowhill@users.noreply.github.com>
2017-12-30 12:49:37 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-gcc -g -o sample01_scaning sample01_scaning.c -I"/opt/ffmpeg/include" $(pkg-config --libs libavformat libavcodec libavutil);
+gcc -g -o sample01_scanning sample01_scanning.c -I"/opt/ffmpeg/include" $(pkg-config --libs libavformat libavcodec libavutil);
 gcc -g -o sample02_demuxing sample02_demuxing.c -I"/opt/ffmpeg/include" $(pkg-config --libs libavformat libavcodec libavutil);
 gcc -g -o sample03_remuxing sample03_remuxing.c -I"/opt/ffmpeg/include" $(pkg-config --libs libavformat libavcodec libavutil);
 gcc -g -o sample04_decoding sample04_decoding.c -I"/opt/ffmpeg/include" $(pkg-config --libs libavformat libavcodec libavutil);
